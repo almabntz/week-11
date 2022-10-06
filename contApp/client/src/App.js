c8008caac37d3c7d559d0d4c6db0d3d61a6cca7b
Alma Benitez <alma.bntz@gmail.com>
2022-10-06 13:49:36 -0500

    js logic

---
+++ contApp/client/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Contacts from "./components/contacts";
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello from Techtonica</h1>
+      <Contacts/>
+    </div>
+  );
+}
+
+export default App;
